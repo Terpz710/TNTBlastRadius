8b760f380b03f851e8d07a51ec8f6f310c9cb71c
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-09-19 14:10:28 -0700

    Update TNTCommand.php

--- src/Terpz710/TNTBlastRadius/TNTCommand.php
+++ src/Terpz710/TNTBlastRadius/TNTCommand.php
@@ -8,15 +8,19 @@ use pocketmine\player\Player;
 
 class TNTCommand extends Command {
 
-    public function __construct() {
+    private $plugin;
+
+    public function __construct(Main $plugin) {
         parent::__construct("tntradius", "Adjust the TNT blast radius");
         $this->setPermission("tntradius.command");
         $this->setAliases(["tntedit"]);
+        $this->plugin = $plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if ($sender instanceof Player) {
-            TNTForm::execute($sender); // Use the correct namespace for TNTForm
+            $form = new TNTForm();
+            $form->execute($sender, $this->plugin);
         } else {
             $sender->sendMessage("This command can only be used in-game.");
         }
