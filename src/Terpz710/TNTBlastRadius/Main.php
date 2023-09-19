08a9e1b6d4ace1ed5a05a42d8a5a436e52ec8600
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-09-19 13:00:02 -0700

    Update Main.php

--- src/Terpz710/TNTBlastRadius/Main.php
+++ src/Terpz710/TNTBlastRadius/Main.php
@@ -3,26 +3,31 @@
 namespace Terpz710\TNTBlastRadius;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityPreExplodeEvent;
 use pocketmine\entity\object\PrimedTNT;
+use pocketmine\Player;
+use Terpz710\TNTBlastRadius\TNTCommand;
+use Terpz710\TNTBlastRadius\TNTForm;
 
 class Main extends PluginBase implements Listener {
 
+    private $pendingRadiusChange = [];
+
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveDefaultConfig();
+        $this->getServer()->getCommandMap()->register("tntradius", new TNTCommand($this));
     }
 
     public function onExplosionPrime(EntityPreExplodeEvent $event) {
         $tnt = $event->getEntity();
         if ($tnt instanceof PrimedTNT) {
-            $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-            $blastRadius = $config->get("blast-radius", 4); // Default blast radius is 4.
-            
-            $scaledRadius = max(1, min(8, $blastRadius));
-            $event->setRadius($scaledRadius);
+            if (isset($this->pendingRadiusChange[$event->getPlayer()->getName()])) {
+                $radius = $this->pendingRadiusChange[$event->getPlayer()->getName()];
+                $scaledRadius = max(1, min(25, $radius));
+                $event->setRadius($scaledRadius);
+                unset($this->pendingRadiusChange[$event->getPlayer()->getName()]);
+            }
         }
     }
 }
