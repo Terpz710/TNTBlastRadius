ac5f8a1ecc79f960da4c9ed91ab29c3e13267513
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-09-20 21:02:30 -0700

    Update TNTCommand.php

--- src/Terpz710/TNTBlastRadius/Command/TNTCommand.php
+++ src/Terpz710/TNTBlastRadius/Command/TNTCommand.php
@@ -5,20 +5,19 @@ namespace Terpz710\TNTBlastRadius\Command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\plugin\PluginOwned;
 use pocketmine\plugin\PluginOwnedTrait;
 use Terpz710\TNTBlastRadius\Main;
 
-class TNTCommand extends Command implements PluginOwned {
+class TNTCommand extends Command {
     use PluginOwnedTrait;
 
     public function __construct(Main $plugin) {
         parent::__construct("tntradius", "Adjust the TNT blast radius");
         $this->setPermission("tntradius.command");
         $this->setAliases(["tntedit"]);
-        $this->initPlugin($plugin);
+        $this->setPlugin($plugin);
     }
-    
+
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if ($sender instanceof Player) {
             $this->getPlugin()->openRadiusSelectorUI($sender);
